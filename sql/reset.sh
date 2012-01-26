17804980ea2108d922bfec2d012a75cc89f2cb49
Chad Whitacre <chad@zetaweb.com>
2012-01-25 22:20:20 -0500

    Update README.

--- sql/reset.sh
+++ sql/reset.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -e
+echo "sudo password, then postgres password ..."
 sudo -u postgres psql < 0000.sql 
+echo "postgres password again ..."
 sudo -u postgres psql -d ihazmoney < 0001.sql 
+echo "and again ..."
 sudo -u postgres psql -d ihazmoney < 0002.sql 
