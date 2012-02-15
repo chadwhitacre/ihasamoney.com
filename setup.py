a81c066168bce1f7c022bd53da1f6f94b0c48f21
Chad Whitacre <chad@zetaweb.com>
2012-02-15 09:42:05 -0500

    Still trying to pin down 43475.

--- setup.py
+++ setup.py
@@ -2,13 +2,8 @@ from ez_setup import use_setuptools
 use_setuptools()
 
 from setuptools import find_packages, setup
-version = "n/a"
 
 
-setup( author = 'Chad Whitacre'
-     , author_email = 'chad@zetaweb.com'
-     , description = ('IHazMoney.com is a website.')
-     , name = 'ihazmoney'
+setup( name = 'ihazmoney'
      , packages = find_packages()
-     , version = version
       )
