81a57ac6cde46e8fdccbc19d19aaf75541d551fe
Chad Whitacre <chad@zetaweb.com>
2012-02-15 09:31:42 -0500

    https://support.heroku.com/requests/43475

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
-from setuptools import setup, find_packages
+from ez_setup import use_setuptools
+use_setuptools()
 
-setup( name='IHazMoney'
-     , packages=find_packages()
+from setuptools import find_packages, setup
+version = "n/a"
+
+
+setup( author = 'Chad Whitacre'
+     , author_email = 'chad@zetaweb.com'
+     , description = ('IHazMoney.com is a website.')
+     , name = 'ihazmoney'
+     , packages = find_packages()
+     , version = version
+     , zip_safe = False
       )
