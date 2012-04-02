7faf165ad80a697f678fb6661069fba7d46e3dd6
Chad Whitacre <chad@zetaweb.com>
2012-04-01 23:47:40 -0400

    Ah, forget to wire up in cli

--- ihasamoney/cli.py
+++ ihasamoney/cli.py
@@ -5,4 +5,8 @@ import ihasamoney.billing
 
 
 def iham_bill():
+    ihasamoney.wire_amount()
+    ihasamoney.wire_db()
+    ihasamoney.wire_samurai()
+
     print "Greetings, program! We're charging people $%s." % ihasamoney.amount
