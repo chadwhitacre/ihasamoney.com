1e159896f6fa7a928751be4fcb04449988d09675
Chad Whitacre <chad@zetaweb.com>
2012-04-01 21:35:09 -0400

    Baby step on billing
    There is a scheduler addon for heroku that I'm going to try using for recurring
    billing. Here is a minimally working script to test out the scheduler addon.

---
+++ ihasamoney/cli.py
@@ -0,0 +1,6 @@
+"""This is installed as `iham`.
+"""
+import sys
+
+def iham_bill():
+    print "Greetings, program!"
--- setup.py
+++ setup.py
@@ -4,4 +4,7 @@ from ihasamoney import __version__
 setup( name='IHasAMoney'
      , packages=find_packages()
      , version=__version__
+     , entry_points = { 'console_scripts'
+                      : ['iham-bill = ihasamoney.cli:iham_bill']
+                       }
       )
